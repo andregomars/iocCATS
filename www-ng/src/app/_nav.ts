95bab5dda2606abb71c652001660f6810cd816bc
andregomars <andregomars@gmail.com>
2018-01-17 23:41:42 -0800

    set menus

--- www-ng/src/app/_nav.ts
+++ www-ng/src/app/_nav.ts
@@ -14,8 +14,65 @@ export const navigation = [
     icon: 'icon-home',
     badge: {
       variant: 'info',
-      text: 'NEW'
+      text: 'Start'
     }
+  },
+  {
+    name: 'Fleet',
+    url: '/home',
+    icon: 'fa fa-bus'
+  },
+  {
+    name: 'Alert',
+    url: '/home',
+    icon: 'icon-bell'
+  },
+  {
+    name: 'Preventive Maint',
+    url: '/home',
+    icon: 'icon-umbrella'
+  },
+  {
+    name: 'Data Log',
+    url: '/datalog',
+    icon: 'icon-event',
+    children: [
+      {
+        name: 'Maintenance',
+        url: '/home',
+        badge: {
+          variant: 'secondary',
+          text: 'LOG'
+        }
+      },
+      {
+        name: 'Daily Routing',
+        url: '/home',
+        badge: {
+          variant: 'secondary',
+          text: 'LOG'
+        }
+      },
+      {
+        name: 'Connection',
+        url: '/home',
+        badge: {
+          variant: 'secondary',
+          text: 'LOG'
+        }
+      },
+
+    ]
+  },
+  {
+    name: 'Monthly Report',
+    url: '/home',
+    icon: 'icon-graph'
+  },
+  {
+    name: 'Notification Set',
+    url: '/home',
+    icon: 'icon-note'
   }
 
 ];
