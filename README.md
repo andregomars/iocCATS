420858a595b8eb7557fddf90fd891ae1233229f9
andregomars <andregomars@gmail.com>
2018-01-18 00:43:05 -0800

    add tabs in home page

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # iocCATS
+
+* Build angular bundles
+```bash
+docker run -it --rm -v "$(pwd)":/home/node andregomars/angular-cli sh
+```
--- www-ng/src/app/views/home/home.module.ts
+++ www-ng/src/app/views/home/home.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 
+// Tabs Component
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   imports: [
     HomeRoutingModule,
-    ChartsModule
+    ChartsModule,
+    TabsModule
   ],
   declarations: [ HomeComponent ]
 })
