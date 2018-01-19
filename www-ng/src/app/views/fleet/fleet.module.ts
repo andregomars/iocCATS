cc46ff5167172c2f2eea123ddbf9dd07b182b46c
andregomars <andregomars@gmail.com>
2018-01-19 08:22:07 -0800

    add ngx datatable in fleet page

--- www-ng/src/app/views/fleet/fleet.module.ts
+++ www-ng/src/app/views/fleet/fleet.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild, Component } from '@angular/core';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { AgmCoreModule } from '@agm/core';
 import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
+import { NgxDatatableModule, DatatableComponent } from '@swimlane/ngx-datatable';
 
 import { FleetComponent } from './fleet.component';
 import { FleetRoutingModule } from './fleet-routing.module';
@@ -10,6 +11,7 @@ import { FleetRoutingModule } from './fleet-routing.module';
   imports: [
     FleetRoutingModule,
     ChartsModule,
+    NgxDatatableModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyC2aUGq0zuZMLTgrUG72Wb4LX6nOA_Q4VM'
     }),
@@ -17,4 +19,51 @@ import { FleetRoutingModule } from './fleet-routing.module';
   ],
   declarations: [ FleetComponent ]
 })
-export class FleetModule { }
+export class FleetModule { 
+
+  rows = [];
+
+  temp = [];
+
+  columns = [
+    { prop: 'name' },
+    { name: 'Company' },
+    { name: 'Gender' }
+  ];
+  @ViewChild(DatatableComponent) table: DatatableComponent;
+
+  constructor() {
+    this.fetch((data) => {
+      // cache our list
+      this.temp = [...data];
+
+      // push our inital complete list
+      this.rows = data;
+    });
+  }
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/data/company.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
+
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function(d) {
+      return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
+  }
+}
