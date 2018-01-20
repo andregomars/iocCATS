2420cb3549a9aa95f0cea1b659899fe3d396db2e
andregomars <andregomars@gmail.com>
2018-01-19 19:37:42 -0800

    add fleet table

--- www-ng/src/app/views/fleet/fleet.module.ts
+++ www-ng/src/app/views/fleet/fleet.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ViewChild, Component } from '@angular/core';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { AgmCoreModule } from '@agm/core';
 import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
-import { NgxDatatableModule, DatatableComponent } from '@swimlane/ngx-datatable';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { FleetComponent } from './fleet.component';
 import { FleetRoutingModule } from './fleet-routing.module';
@@ -19,51 +19,4 @@ import { FleetRoutingModule } from './fleet-routing.module';
   ],
   declarations: [ FleetComponent ]
 })
-export class FleetModule { 
-
-  rows = [];
-
-  temp = [];
-
-  columns = [
-    { prop: 'name' },
-    { name: 'Company' },
-    { name: 'Gender' }
-  ];
-  @ViewChild(DatatableComponent) table: DatatableComponent;
-
-  constructor() {
-    this.fetch((data) => {
-      // cache our list
-      this.temp = [...data];
-
-      // push our inital complete list
-      this.rows = data;
-    });
-  }
-
-  fetch(cb) {
-    const req = new XMLHttpRequest();
-    req.open('GET', `assets/data/company.json`);
-
-    req.onload = () => {
-      cb(JSON.parse(req.response));
-    };
-
-    req.send();
-  }
-
-  updateFilter(event) {
-    const val = event.target.value.toLowerCase();
-
-    // filter our data
-    const temp = this.temp.filter(function(d) {
-      return d.name.toLowerCase().indexOf(val) !== -1 || !val;
-    });
-
-    // update the rows
-    this.rows = temp;
-    // Whenever the filter changes, always go back to the first page
-    this.table.offset = 0;
-  }
-}
+export class FleetModule { }
