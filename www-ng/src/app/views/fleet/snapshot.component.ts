66f213377740ff419290720ba86320a54c647626
andregomars <andregomars@gmail.com>
2018-02-22 08:54:02 -0800

    add indicator icons

--- www-ng/src/app/views/fleet/snapshot.component.ts
+++ www-ng/src/app/views/fleet/snapshot.component.ts
@@ -4,7 +4,8 @@ import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  templateUrl: 'snapshot.component.html'
+  templateUrl: 'snapshot.component.html',
+  styleUrls: [ 'snapshot.component.scss' ]
 })
 export class SnapshotComponent implements OnInit {
 
@@ -17,13 +18,22 @@ export class SnapshotComponent implements OnInit {
   private alert$: Observable<any>;
   private locations$: Observable<any>;
 
-  private modules$: Observable<any>;
+  private moduleIcons$: Observable<any>;
   private snapshots$: Observable<any>;
 
   private colsSnapshot = [
     { name: 'Item', prop: 'item' },
     { name: 'Value', prop: 'value' },
     { name: 'Unit', prop: 'unit' }
+  ];
+
+  private icons = [
+    { name: 'ENGINE', path: 'assets/img/indicators/engine' },
+    { name: 'TRANSMISSION', path: 'assets/img/indicators/transmission' },
+    { name: 'ABS', path: 'assets/img/indicators/abs' },
+    { name: 'HVAC', path: 'assets/img/indicators/hvac' },
+    { name: 'EQULIZER', path: 'assets/img/indicators/equlizer' },
+    { name: 'FAN', path: 'assets/img/indicators/fan' },
   ];
 
   ngOnInit(): void {
@@ -46,7 +56,29 @@ export class SnapshotComponent implements OnInit {
       // .map(v => v['gps_location']);
       .switchMap(v => Observable.from(v['gps_location']));
 
-    this.modules$ = this.alert$.map(a => a.module_info);
     this.snapshots$ = this.alert$.map(a => a.item_info);
+    this.moduleIcons$ = this.alert$.map(a => this.mapIconPaths(a.module_info));
+  }
+
+  mapIconPaths(modules: Array<any>): Array<string> {
+    return this.icons.map(icon => {
+      let path = 'assets/img/indicators/engine_na.png';
+
+      try {
+        const modules_filtered = modules.filter(module =>
+          icon.name === module.module_name.toUpperCase());
+        if (modules_filtered.length > 0) {
+          if (modules_filtered[0].module_status === 'on') {
+            path = `${ icon.path }_online.png`;
+          } else {
+            path = `${ icon.path }_offline.png`;
+          }
+        } else {
+            path = `${ icon.path }_na.png`;
+        }
+      } catch { }
+
+      return path;
+    });
   }
 }
