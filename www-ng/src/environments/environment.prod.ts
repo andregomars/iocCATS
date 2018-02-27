1fa60309cef570d63f42c87fc65173aac07bdb99
andregomars <andregomars@gmail.com>
2018-02-26 21:29:36 -0800

    adjust environment file generate rule

--- www-ng/src/environments/environment.prod.ts
+++ www-ng/src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
+export enum DataSourceType {
+  Local,
+  Firebase,
+  Swagger
+}
+
 export const environment = {
-  production: true
+  production: true,
+  agm: { apiKey: '' },
+  firebase: {
+    apiKey: '',
+    authDomain: 'ioccatsdemo.firebaseapp.com',
+    databaseURL: 'https://ioccatsdemo.firebaseio.com',
+    projectId: 'ioccatsdemo',
+    storageBucket: 'ioccatsdemo.appspot.com',
+    messagingSenderId: '652898326248'
+  },
+  apiRootLocal: 'assets/data',
+  apiRootIocCATS: 'http://localhost:8080/api/rest/v1',
+  dataSource: DataSourceType.Swagger
 };
